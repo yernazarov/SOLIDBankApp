da57eac6badbb611ddb73349746e0627443228d8
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-05-24 08:41:03 +0600

    Add @Repository annotations

--- src/main/java/com/zhandos/SOLIDBankApp/account/AccountDAO.java
+++ src/main/java/com/zhandos/SOLIDBankApp/account/AccountDAO.java
@@ -5,9 +5,11 @@ import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
+@Repository
 public interface AccountDAO extends CrudRepository<Account, Long>{
 
     @Query("SELECT * FROM Account WHERE client_id = :clientID")
